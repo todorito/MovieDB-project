50036a7e003c75941dde605d6d9902d7963c0613
Dora <dorastegkou@gmail.com>
2024-01-06 20:53:20 +0200

    fixed search functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,22 +12,22 @@ const SearchBar = function ({ text }) {
   console.log(text);
   const getMovies = function (e) {
     e.preventDefault();
-    if (e.code === "Enter") {
-      navigateToSearch(input);
-    }
+    navigateToSearch(input);
   };
 
   return (
-    <div className="relative m-3 rounded mt-0 background-neutral">
+    <form
+      onSubmit={getMovies}
+      className="relative m-3 rounded mt-0 background-neutral"
+    >
       <input
         placeholder="Search..."
         className=" text-start text-inverted-weak bg-transparent px-5 py-2 w-full pr-10"
         onChange={(e) => setInput(e.target.value)}
-        onKeyUp={getMovies}
         value={input}
       />
       <CiSearch className="text-white absolute top-0 right-5 h-full" />
-    </div>
+    </form>
   );
 };
 
