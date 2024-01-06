0941ea2eec5769ec62e487aac9e1b1305102f33f
Dora <dorastegkou@gmail.com>
2024-01-06 20:53:48 +0200

    Changed modal text

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,19 +4,51 @@ import { IoClose } from "react-icons/io5";
 
 const Modal = function ({ handleModal }) {
   return ReactDOM.createPortal(
-    <div>
+    <div className="flex md:justify-center items-end md:items-center inset-0 fixed">
       <div
         onClick={() => handleModal()}
         className="inset-0 fixed bg-gray-200 opacity-60"
       ></div>
       <div
-        className="border-2 border-gray-300 md:inset-[30%] md:inset-y-[40%] inset-x-[10%] inset-y-[30%]
-       fixed bg-white opacity-1 m-auto p-3 md:pt-[3%] pt-20 text-center"
+        className="border-2 border-gray-300
+       relative bg-white opacity-1 md:w-[50%] p-5 text-center"
       >
-        Hello stranger! This is your first visit in our site and we wanted to
-        welcome you! You can find info for movies and series that you like and
-        add them to your favourites.
-        <div className="absolute right-0 bottom-0">
+        <h3>Hello stranger! 👋</h3>
+        <p>This is your first visit to my site and I wanted to welcome you! </p>
+        <p>
+          You can search for movies that you like -- maybe series in the future
+          -- and add them to your favourites.
+        </p>
+        <p>
+          Please keep in mind that this is a pet project of mine and it&#39;s
+          still a <strong>work in progress</strong>! It&#39;s meant only for
+          demonstration purposes. It&#39;s written in{" "}
+          <a
+            className="text-info underline"
+            href="https://react.dev/"
+            target="_blank"
+          >
+            React.js
+          </a>{" "}
+          and{" "}
+          <a
+            className="text-info underline"
+            href="https://tailwindcss.com/"
+            target="_blank"
+          >
+            tailwindCss
+          </a>
+          . The source code can be found at{" "}
+          <a
+            className="text-info underline"
+            href="https://github.com/todorito/MovieDB-project"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+        <div className="absolute right-0 top-0">
           <Button danger onClick={() => handleModal()}>
             <IoClose />
           </Button>
