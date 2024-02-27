19e17aa42ce900220f976bc7e0d1ffbabb41bb6f
Dora <dorastegkou@gmail.com>
2024-02-27 11:54:48 +0200

    Added FavouriteSeriesPage

---
+++ src/pages/FavouriteSeriesPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import MovieInfo from "../components/MovieInfo";
+import { useFavourites } from "../providers/favouriteProvider";
+
+const FavouriteSeriesPage = function () {
+  const { favData } = useFavourites();
+
+  const allFavourites =
+    favData &&
+    favData.map((item, index) => {
+      return <MovieInfo key={item.id} item={item} index={index} />;
+    });
+
+  return (
+    <div className="min-h-full">
+      <h1 className="container mx-auto text-white text-2xl w-auto px-[2%] flex justify-start mt-5">
+        Your favourite Series
+      </h1>
+      <div className="container mx-auto flex flex-wrap flex-row justify-start">
+        {allFavourites}
+      </div>
+    </div>
+  );
+};
+
+export default FavouriteSeriesPage;
