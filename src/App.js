3c16082d30d93940915ff7b19f68ecb8d456e0ee
Dora <dorastegkou@gmail.com>
2025-04-25 16:18:57 +0300

    Added route for Person Details Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FavouritesPage from "./pages/FavouriteMoviesPage";
 import SearchPage from "./pages/SearchPage";
 import ErrorPage from "./pages/ErrorPage";
 import MovieDetailsPage from "./pages/MovieDetailsPage";
+import PersonDetailsPage from "./pages/PersonDetailsPage";
 import { FavouriteProvider } from "./providers/favouriteProvider";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -14,12 +15,13 @@ function App() {
     <Router>
       <FavouriteProvider>
         <div className="background-neutral-strong min-h-screen">
-        <Header />
+          <Header />
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/favourites" element={<FavouritesPage />} />
             <Route path="/search" element={<SearchPage />} />
             <Route path="/movieDetails/:id" element={<MovieDetailsPage />} />
+            <Route path="/personDetails/:id" element={<PersonDetailsPage />} />
             <Route path="*" element={<ErrorPage />} />
           </Routes>
         </div>
